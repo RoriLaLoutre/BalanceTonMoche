a0be19fcd1a37086a199a98bda9844cce85f72be
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-01-08 15:02:15 +0100

    initial commit

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+ const DB_CONFIG = [
+    'db' => 'mysql',
+    'host' => 'localhost',
+    'port' => '3307',
+    'dbname' => '', 
+    'username' => 'root',
+    'password' => '', 
+ ];
+
---
+++ config/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+const AVLAIBLE_ROUTES = [
+	"home" => ["template" => "homeController.php",
+                "seo" => [
+                    "title" => "Home",
+                    "description" => "Page home"
+                ]],
+   
+
+];
+
+const DEFAULT_ROUTE = AVLAIBLE_ROUTES['404'];
