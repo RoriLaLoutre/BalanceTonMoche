ef4b40348bc2d05152177e9ac942a39eacd7df6b
Russias <russiasesteban@gmail.com>
2025-01-08 18:21:02 +0100

    add home content

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,14 @@
 <div class="home">
-    <h1>Home</h1>
+    <h2>Vous en avez marre de croiser que des moches ?</h2>
+    <p>
+        C’est l’heure de prendre les choses en main !
+        Grâce à notre app révolutionnaire, signalez les spécimens qui vous font froncer les sourcils.
+         Un simple clic, une photo, et hop, vous contribuez à une base de données mondiale de la beauté… ou pas ! 
+    </p>
+    <p>
+        Parce qu’il est temps de rétablir l’équilibre visuel dans nos vies. Alors, prêt à jouer votre rôle ?
+    </p>
+    <div class="zone-btn">
+        <a href="index?page=poucave" id="btn-poucave">Délafion</a>
+    </div>
 </div>
