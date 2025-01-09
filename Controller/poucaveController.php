459cff5b8c77417883e5c1116a3b86618404ed53
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-01-09 09:52:41 +0100

    modif poucave controller

--- Controller/poucaveController.php
+++ Controller/poucaveController.php
@@ -1,8 +1,42 @@
 <?php
 require_once('./models/CrudManager.php');
 
-
 $template = './views/pages/poucave.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['categorie'])) {
+        $categorie = htmlspecialchars($categorie);
+    } else {
+        $categorie = ''; 
+    }
+
+    if (isset($_POST['nom'])) {
+        $nom = htmlspecialchars($nom);
+    } else {
+        $nom = ''; 
+    }
+
+    if (isset($_POST['prenom'])) {
+        $prenom = htmlspecialchars($prenom);
+    } else {
+        $prenom = ''; 
+    }
+    
+    if (isset($_FILES['photo'])) {
+            $file = $_FILES['photo'];
+            $filePath = $file['tmp_name'];
+            move_uploaded_file($filePath, './public/specimenpics/' . $file['name']);
+    } else {
+        $file = ''; 
+    }   
+
+    $pos_lat = 0;
+    $pos_long = 0;
+
+
+    addMoche($nom , $prenom,'./public/specimenpics/' . $file['name'] , $categorie , $note , $pos_long , $pos_lat);
+}
 
 
 
--- views/pages/poucave.php
+++ views/pages/poucave.php
@@ -1,1 +1,38 @@
-<h1>poucave</h1>
+<div>
+
+    <h1>
+        Voici la page de dénonciation pour trouble à l'esthétique publique
+    </h1>
+
+    <form action="" method="POST">
+
+        <label for="photo">Uploader la photo</label>
+        <input type="file" name = "photo" id="photo">
+
+        <label for="nom">Nom du spéciment si celui-ci vous est familié</label>
+        <input type="text" name = "nom" id="nom">
+
+        <label for="prenom">Prenom du spéciment si celui-ci vous est familié</label>
+        <input type="text" name = "prenom" id="prenom">
+
+
+        <select name="categorie" id="categorie">
+            <option value="">Choisir une catégorie</option>
+            <option value="corp">Corps</option>
+            <option value="pied">Pied</option>
+            <option value="main">Main</option>
+            <option value="dents">Dents</option>
+            <option value="malformation">Malformation</option>
+            <option value="visage">Visage</option>
+            <option value="cheveux">Cheveux</option>
+            <option value="global">Esthétique globale</option>
+            <option value="other">Autres...</option>
+        </select>
+
+        <input type="submit" text="Soumettre">
+
+    </form>
+
+
+
+</div>
