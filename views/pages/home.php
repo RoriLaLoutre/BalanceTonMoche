4a9a7b37b77b6bd7b1cc8bb357f3dc1d2ba1036d
Russias <russiasesteban@gmail.com>
2025-01-09 08:13:42 +0100

    Text update

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,13 +1,25 @@
 <div class="home">
-    <h2>Vous en avez marre de croiser que des moches ?</h2>
+    <!-- <h2>Vous en avez marre de croiser que des moches ?</h2>
     <p>
         C’est l’heure de prendre les choses en main !
         Grâce à notre app révolutionnaire, signalez les spécimens qui vous font froncer les sourcils.
          Un simple clic, une photo, et hop, vous contribuez à une base de données mondiale de la beauté… ou pas ! 
+    </p> -->
+    <h2>
+        Bienvenue sur le portail officiel de signalement des normes esthétiques publiques
+    </h2>
+    <p>
+        Dans le cadre de l'application de l'Article <span>L.999-1</span> du Code fictif de l'esthétique publique, ce site permet à chaque citoyen de contribuer à maintenir l'harmonie visuelle de notre société en signalant toute personne contrevenant aux standards esthétiques définis par le Haut Conseil de l'Harmonie Visuelle.
+    </p> 
+    <p>
+        En tant que garant de la beauté collective, vous êtes invité à faire un signalement anonyme si vous observez une non-conformité manifeste aux critères esthétiques en vigueur. Ce dispositif vise à assurer le respect des normes et à préserver l'unité visuelle de notre espace public.
     </p>
     <p>
-        Parce qu’il est temps de rétablir l’équilibre visuel dans nos vies. Alors, prêt à jouer votre rôle ?
+        Tous les signalements seront examinés par les autorités compétentes et feront l'objet d'une étude approfondie.
     </p>
+    <h3>
+        Votre engagement pour l'esthétique publique est essentiel !
+    </h3>
     <div class="zone-btn">
         <a href="index?page=poucave" id="btn-poucave">Délafion</a>
     </div>
