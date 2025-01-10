5b9fd69163a366cb78deef5393f506edb4f2ec72
Russias <russiasesteban@gmail.com>
2025-01-10 10:23:22 +0100

    wanted

--- views/pages/analyse.php
+++ views/pages/analyse.php
@@ -1,3 +1,17 @@
-<h1>
-    analyse
-    </h1>
+<div class="analyse">
+        <h2>Quelques stats :</h2>
+        <div class="stat-1">
+            <?php
+            foreach($lastSignalements as $the_report){ ?>
+            <div class="card-report">
+                <img src="<?= $the_report["img"]?>" alt="Report_<?= $the_report["id"]?>_<?= $the_report["nom"]?>_<?= $the_report["prenom"]?>">
+                <p>
+                    <?= $the_report["note"]?>
+                </p>
+            </div>
+            <?php
+            }
+            ?>
+        </div>
+       
+    </div>
