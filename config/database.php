0fbb32549b3393968833b3faf12f40b718e06af2
Mathis <mathislegamin@gmail.com>
2025-01-14 17:25:50 +0100

    database

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
  const DB_CONFIG = [
     'db' => 'mysql',
     'host' => 'localhost',
-    'port' => '3306',
+    'port' => '3307',
     'dbname' => 'btm', 
     'username' => 'root',
     'password' => '', 
