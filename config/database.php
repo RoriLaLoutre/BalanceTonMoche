c0b140eed70663fe76790ef0d1591c7c53f26ded
Mathis <mathislegamin@gmail.com>
2025-01-14 15:11:28 +0100

    fix finis

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
  const DB_CONFIG = [
     'db' => 'mysql',
     'host' => 'localhost',
-    'port' => '3307',
+    'port' => '3306',
     'dbname' => 'btm', 
     'username' => 'root',
     'password' => '', 
