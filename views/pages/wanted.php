e89383bc92a4a1936a4243abb579fa40cf359304
Russias <russiasesteban@gmail.com>
2025-01-15 17:13:19 +0100

    Refonte de wanted

--- views/pages/wanted.php
+++ views/pages/wanted.php
@@ -1,31 +1,27 @@
 <div class="wanted">
-        <h2>Les plus recherchés :</h2>
-        <div class="zone-wanted">
-            <?php
-            foreach($lastSignalements as $the_report){ ?>
-            <div class="card-report">
-
-                <p>
-                    <?= $the_report["note"]?> / 10
-                </p>
-                <p class="rank">
-                    <?=titre($the_report["note"],$dangerLvl);?>
-                </p>
-                <?php 
-                $prime = prime($the_report["note"], $dangerLvl);
-                if ($prime > 0) { ?>
-                    <p class="prime">
-                        Prime: <?= $prime; ?> €
-                    </p>
-                <?php } ?>
-                <img src="<?= $the_report["img"]?>" alt="Report_<?= $the_report["id"]?>_<?= $the_report["nom"]?>_<?= $the_report["prenom"]?>">
-                <div class="zone-btn">
-                    <a href="#" id="btn-id" class ="btn-bleu">A propos</a>
-                </div>
+    <h2>Les plus recherchés :</h2>
+    <header id="wanted-header">
+        <?php foreach ($lastSignalements as $the_report) { ?>
+        <div class="box-wanted">
+            <!-- Haut gauche -->
+            <div class="top-left">
+                <?= $the_report["note"] ?>
             </div>
-            <?php
-            }
-            ?>
+            <!-- Haut droite -->
+            <div class="top-right">
+                <img src="<?= $the_report["img"] ?>" alt="Report_<?= $the_report["id"] ?>_<?= $the_report["nom"] ?>_<?= $the_report["prenom"] ?>">
+            </div>
+            <!-- Bas gauche -->
+            <div class="bottom-left">
+                <p><?= $the_report["nom"] ?></p>
+                <p><?= $the_report["prenom"] ?></p>
+                <p><?= titre($the_report["note"], $dangerLvl); ?></p>
+            </div>
+            <!-- Bas droite -->
+            <div class="bottom-right" id="map">
+                
+            </div>
         </div>
-       
-    </div>
+        <?php } ?>
+    </header>
+</div>
